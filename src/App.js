934f05ab8aaf90cd7e8964d1238592aeb6bd1b81
IidaPeltonen <n0peii00@students.oamk.fi>
2021-12-06 23:39:13 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState} from 'react'
+import axios from 'axios';
 
-function App() {
+const URL = 'http://localhost/imdb/'
+
+function App () {
+  const [leffat, setLeffat] = useState([]);
+
+  function etsi(e) {
+    e.preventDefault();
+    axios
+      .get(URL)
+      .then(response => {
+        setLeffat(response.data)
+        console.log(response.data)
+      })
+      .catch(error => {
+        alert(error)
+      })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid'>
+      <button onClick={etsi} >PAINA TÄSTÄ</button>
+      <ol>
+         {leffat?.map(leffa => (
+          <li key={leffa.title_id}>
+            <p>{leffa.primary_title}</p>
+            <p>{leffa.start_year}</p>
+            <p>{leffa.average_rating}</p>
+            
+          </li>
+        ))}  
+
+        </ol>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
