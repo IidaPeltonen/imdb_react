78df8383caa3e308633f3c93b69f97f7e4c465c5
IidaPeltonen <n0peii00@students.oamk.fi>
2021-12-07 00:42:35 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const URL = 'http://localhost/imdb/'
 
 function App () {
   const [leffat, setLeffat] = useState([]);
+  const [lyhyet, setLyhyet] = useState([]);
 
   function etsi(e) {
     e.preventDefault();
@@ -20,22 +21,55 @@ function App () {
       })
   }
 
+  function etsiLyhyet(e) {
+    e.preventDefault();
+    axios
+      .get(URL + 'haku.php')
+      .then(response => {
+        setLyhyet(response.data)
+        console.log(response.data)
+      })
+      .catch(error => {
+        alert(error)
+      })
+  }
+
 
   return (
     <div className='container-fluid'>
-      <button onClick={etsi} >PAINA TÄSTÄ</button>
+      <h1 id="center">Kaikki mitä olet halunnut IMDB:ltä!</h1>
+      <div className="row">
+        <div className="col-6">
+      <br />
+      <button onClick={etsi} >Paina tästä löytääkseni parhaat Bean-leffat!</button>
+      <br />
       <ol>
          {leffat?.map(leffa => (
           <li key={leffa.title_id}>
-            <p>{leffa.primary_title}</p>
-            <p>{leffa.start_year}</p>
-            <p>{leffa.average_rating}</p>
-            
+            <b><p>{leffa.primary_title}</p></b>
+            <p>Vuosi: {leffa.start_year}</p>
+            <p>Arvosana: {leffa.average_rating}</p>
+          </li>
+        ))}  
+      </ol>
+      </div>
+      <div className="col-6">
+      <br />
+      <button onClick={etsiLyhyet} >Paina tästä löytääkseni lyhyitä action-leffoja suomenkielisillä julkaisunimillä!</button>
+      <br />
+      <ol>
+        {lyhyet?.map(lyhyt => (
+          <li key={lyhyt.title_id}>
+            <b><p>{lyhyt.primary_title}</p></b>
+            <p>Vuosi: {lyhyt.start_year}</p>
+            <p>Pituus: {lyhyt.runtime_minutes}</p>      
           </li>
         ))}  
 
         </ol>
-    </div>
+        </div>
+        </div>
+</div>
   )
 }
 
